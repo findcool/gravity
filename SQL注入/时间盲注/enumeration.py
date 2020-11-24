e5a92f4c4ab9324221ed035dbc0fcce44ff951af
hoschen <29953808+hosch3n@users.noreply.github.com>
2020-11-24 18:19:09 +0800

    fix bug

--- SQL注入/时间盲注/enumeration.py
+++ SQL注入/时间盲注/enumeration.py
@@ -14,12 +14,12 @@ data = {
 }
 
 headers = {
-    'Origin':  burl,
+    'Origin': burl,
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Cache-Control': 'max-age=0',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 Safari/537.36 Edg/79.0.309.43',
-    'Referer':  f'{burl}/admin539/login.php',
+    'Referer': f'{burl}/admin539/login.php',
     'Connection': 'close',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
